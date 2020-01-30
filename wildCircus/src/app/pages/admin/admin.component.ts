ccd100ea2cd6783d478a9412bbed1c67b61f326b
Antoine Moignet <antoinemoignet@gmail.com>
2020-01-30 13:12:30 +0100

    feat: add some html and css

--- wildCircus/src/app/pages/admin/admin.component.ts
+++ wildCircus/src/app/pages/admin/admin.component.ts
@@ -1,6 +1,9 @@
 import { UserClass } from './../../shared/models/user-class';
 import { Component, OnInit } from '@angular/core';
 import { ReservationService } from '../../shared/services/reservation.service';
+import { TicketService } from '../../shared/services/ticket.service';
+import { TicketClass } from '../../shared/models/ticket-class';
+
 
 @Component({
   selector: 'app-admin',
@@ -10,18 +13,25 @@ import { ReservationService } from '../../shared/services/reservation.service';
 export class AdminComponent implements OnInit {
 
   private users: UserClass[] = [];
+  private tickets: TicketClass[] = [];
 
-  constructor(private reservationService: ReservationService) { }
+  constructor(private reservationService: ReservationService, private ticketService: TicketService) { }
 
   ngOnInit() {
     this.reservationService.getUsers()
     .subscribe(response => this.users = response);
+    this.ticketService.getTicket()
+    .subscribe(data => this.tickets = data);
   }
 
   delete(id: number) {
     this.reservationService.deleteAUser(id)
     .subscribe(response => (this.users.splice(id, 1)));
+    this.ticketService.deleteATicket(id)
+    .subscribe(data => (this.tickets.splice(id, 1)));
   }
+
+
 
   refresh(): void {
     window.location.reload();
